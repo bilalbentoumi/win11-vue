e910ded20a23795e28b80b2fe25df3369c1aec9e
bilalbentoumi <bilalbentoumi@gmail.com>
2021-08-28 01:15:10 +0100

    Add desktop icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,6 +14,14 @@
                 <img src="@/assets/icons/explorer.png" width="43" height="43">
                 <span class="text-white text-xs app-name">File Explorer</span>
             </button>
+            <button class="flex flex-col justify-center items-center w-24 h-28 hover:bg-white hover:bg-opacity-30 rounded-sm duration-200 cursor-auto">
+                <img src="@/assets/icons/store.png" width="43" height="43">
+                <span class="text-white text-xs app-name">Microsoft Store</span>
+            </button>
+            <button class="flex flex-col justify-center items-center w-24 h-28 hover:bg-white hover:bg-opacity-30 rounded-sm duration-200 cursor-auto">
+                <img src="@/assets/icons/edge.png" width="43" height="43">
+                <span class="text-white text-xs app-name">Microsoft Edge</span>
+            </button>
         </div>
 
         <div class="task-bar w-full bg-white bg-opacity-80 backdrop-filter backdrop-blur-lg flex justify-center">
